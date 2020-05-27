9466f65d674f263ad9a12c8a54f4426b6aa9da18
Rajesh Kumar Singh <srajeshkumar@vmware.om>
2020-05-27 16:45:24 +0800

    upd1

--- src/main/java/com/txconsole/HelloController.java
+++ src/main/java/com/txconsole/HelloController.java
@@ -1,12 +1,14 @@
 package com.txconsole;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
 public class HelloController {
 	
-	@GetMapping("/")
+	@GetMapping("/greet")
 	public String greet() {
 		
 		System.out.println("LOG: Hello to all");
---
+++ src/main/java/com/txconsole/HelloUIController.java
@@ -0,0 +1,27 @@
+package com.txconsole;
+
+import java.net.MalformedURLException;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class HelloUIController {
+	
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@GetMapping(value = {"/", "/index"})
+	public String index(Model model, HttpServletRequest request) throws MalformedURLException {
+		
+		return "index";
+	}
+
+}
